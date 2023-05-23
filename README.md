f2d276867cef154240ac0f0cf16c00b76a60ddc5
aprilies <w767760161@gmail.com>
2023-05-23 13:38:14 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tutorial
+
+This folder aims to store my totorial of any programming language or tools I want to utilize.
+
+The menu of this tutorial is below:
+
+- shell
---
+++ shell/hello.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo 'hello' world
